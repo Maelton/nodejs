65240d98576509ea0527b52364d8e94ea71d4221
MAELTON LIMA DOS SANTOS <2022002257@MACEIO.IFAL>
2024-09-20 20:32:33 -0300

    project without express

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const HOST = 'localhost';
+const PORT = 3000;
+
+const requestHandler = (request, response) => {
+    console.log(`Requested URL: ${request.url}`);
+
+    if (request.url === '/static/html/index.html' || request.url == '/') {
+        console.log('Load HTML file');
+        const pathToFile = path.join(__dirname, 'static', 'html', 'index.html');
+        fs.readFile(pathToFile, (error, data) => {
+            if (error) {
+                console.error(error);
+                response.writeHead(404);
+                response.end('Error loading HTML file');
+            } else {
+                response.setHeader('Content-Type', 'text/html');
+                response.writeHead(200);
+                response.end(
+                    data.toString());
+            }
+        });
+    } else if (request.url === '/css/styles.css' || request.url === '/static/css/styles.css') {
+        console.log('Load CSS file');
+        const pathToFile = path.join(__dirname, 'static', 'css', 'styles.css');
+        fs.readFile(pathToFile, (error, data) => {
+            if (error) {
+                console.error(error);
+                response.writeHead(404);
+                response.end('Error loading CSS file');
+            } else {
+                response.setHeader('Content-Type', 'text/css');
+                response.writeHead(200);
+                response.end(data.toString());
+            }
+        });
+    } else {
+        response.writeHead(404);
+        response.end('Error loading file');
+    }
+};
+
+const server = http.createServer(requestHandler);
+server.listen(PORT, HOST, () => {
+    console.log(`Web server running at http://${HOST}:${PORT}`);
+});
